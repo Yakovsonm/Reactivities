e7719f253b1717c676dbd27fb7aac758256f8c75
Michael yakovson <yakovsonm@gmail.com>
2022-08-13 10:45:49 +0300

    End of section 14

---
+++ Persistence/Migrations/20220813055543_AddCancelledProperty.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class AddCancelledProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IsCancelled",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCancelled",
+                table: "Activities");
+        }
+    }
+}
---
+++ Persistence/Migrations/20220813062903_AddCancelledProperty2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class AddCancelledProperty2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsCancelled",
+                table: "Activities",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsCancelled",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+        }
+    }
+}
